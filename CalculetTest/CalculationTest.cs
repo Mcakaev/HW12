758a4c737c39750877c4c68965858ffc86864531
Mcakaev <63405150+Mcakaev@users.noreply.github.com>
2020-04-22 21:44:40 +0300

    Add files via upload
    Programm calculator and calcTest

---
+++ CalculetTest/CalculationTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using calculator1;
+
+namespace CalculatorTest
+{
+    [TestFixture]
+    public class CalculationTest
+    {
+        [TestCase(1,3,4)]
+        [TestCase(-2, -6, -8)]
+        [TestCase(-2, 2, 0)]
+        public void TestSumMethod(double number1, double number2, double expRes)
+        {           
+            double actRes = new Calculculation().Sum(number1,number2);
+            Assert.AreEqual(expRes, actRes);
+        }
+        [TestCase(5, 3, 2)]
+        [TestCase(-11, -2, -9)]
+        [TestCase(-11, -11, 0)]
+        public void TestDiffMethod(double number1, double number2, double expRes)
+        {
+            double actRes = new Calculculation().Diff(number1, number2);
+            Assert.AreEqual(expRes, actRes);
+        }
+        [TestCase(5, 3, 15)]
+        [TestCase(-11, -2, 22)]
+        [TestCase(-11, 2, -22)]
+        [TestCase(-11, 0, 0)]
+        [TestCase(0, 222, 0)]
+        public void TestMultiplyMethod(double number1, double number2, double expRes)
+        {
+            double actRes = new Calculculation().Multiply(number1, number2);
+            Assert.AreEqual(expRes, actRes);
+        }
+        [TestCase(6,2,3)]
+        [TestCase(-3,1,-3)]
+        [TestCase(-3, -1, 3)]
+        [TestCase(3, -1, -3)]
+        [TestCase(0, 1, 0)]        
+        [TestCase(15, 4, 3.75)]
+        [TestCase(2.8, 1.4, 2)]
+        public void TestDivMethod(double number1, double number2, double expRes)
+        {            
+                double actRes = new Calculculation().Division(number1, number2);
+                Assert.AreEqual(expRes, actRes);          
+        }
+        [TestCase(1, 0, "∞")]
+        public void TestDivByZero(double number1, double number2, string expRes)
+        {
+            double actRes = new Calculculation().Division(number1, number2);
+            Assert.AreEqual(expRes, actRes.ToString());
+        }
+    }
+}
---
+++ calculator1/Calculator1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator1
+{
+    class Calculator1
+    {
+        static void Main(string[] args)
+        {
+            double a = EnterNumber1();
+            char operation = EnterOperator();
+            double b = EnterNumber2();
+            Calculculation calc = new Calculculation();
+
+
+            switch (operation)
+            {
+                case '+':
+                    PrdoubleResult(calc.Sum(a, b).ToString());
+                    break;
+                case '-':
+                    PrdoubleResult(calc.Diff(a, b).ToString());
+                    break;
+                case '*':
+                    PrdoubleResult(calc.Multiply(a, b).ToString());
+                    break;
+                case '/':
+                    try
+                    {
+                        PrdoubleResult(calc.Division(a, b).ToString());
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Divide by zero");
+                    }
+                    break;
+                case '%': 
+                    PrdoubleResult(calc.Mod(a, b).ToString());
+                    break;
+            }
+
+        }
+
+        static double EnterNumber1()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter first number:");
+                double num1;
+                if (double.TryParse(Console.ReadLine(), out num1))
+                {
+                    return num1;
+                }
+                else { continue; }
+
+            }
+
+        }
+        static char EnterOperator()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter operator:");
+                char oper = Console.ReadLine()[0];
+                if (oper == '+' || oper == '-' || oper == '*' || oper == '/' || oper == '%')
+                    return oper;
+
+                else
+                {
+                    Console.WriteLine("Wrong input, try again!");
+                    continue;
+                }
+            }
+        }
+        static double EnterNumber2()
+        {
+            while (true)
+            {
+
+                Console.WriteLine("Enter second number:");
+                double num2;
+                if (double.TryParse(Console.ReadLine(), out num2))
+                {
+                    return num2;
+                }
+                else { continue; }
+            }
+        }        
+        static void PrdoubleResult(string result)
+        {
+            Console.WriteLine("\nResult: " + result);
+        }
+    }
+}
